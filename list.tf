d1f7611a09c33deb8fda18496898d5f2bc741146
kumarraiakhilesh <kumarraiakhilesh@gmail.com>
2026-05-07 23:29:38 +0530

    Update list.tf

--- list.tf
+++ list.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "azurerm" {
     features {}
-      subscription_id = ""
+      subscription_id = "9097d0f8-4798-48d3-891f-83da77060e96"
     }  
 resource"azurerm_resource_group" "assinetest" {
     for_each = toset(var.roja)
